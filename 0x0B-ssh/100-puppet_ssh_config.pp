0d20a06f115320614dc44ac7f5f997f939b9f6af
root <mbajily@gmail.com>
2024-06-30 13:48:19 -0700

    fix path to ssh_config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 file_line { 'Turn off passwd auth':
   ensure  => present,
-  path    => '/home/vagrant/.ssh/config',
+  path    => '/etc/.ssh/ssh_config',
   line    => 'PasswordAuthentication no',
   replace => true,
 }
 
 file_line { 'Declare identity file':
   ensure  => present,
-  path    => '/home/vagrant/.ssh/config',
+  path    => '/etc/.ssh/ssh_config',
   line    => 'IdentityFile ~/.ssh/school',
   replace => true,
 }
