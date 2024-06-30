950f8a4fdebf8853f2e4ebe2fe7f634bcdf36b7a
root <mbajily@gmail.com>
2024-06-30 13:49:17 -0700

    fix path to ssh_config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 file_line { 'Turn off passwd auth':
   ensure  => present,
-  path    => '/etc/.ssh/ssh_config',
+  path    => '/etc/ssh/ssh_config',
   line    => 'PasswordAuthentication no',
   replace => true,
 }
 
 file_line { 'Declare identity file':
   ensure  => present,
-  path    => '/etc/.ssh/ssh_config',
+  path    => '/etc/ssh/ssh_config',
   line    => 'IdentityFile ~/.ssh/school',
   replace => true,
 }
