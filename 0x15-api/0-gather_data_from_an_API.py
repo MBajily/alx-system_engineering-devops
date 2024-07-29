3f8bc2527c81fc672e607d65b199a87abf5466ca
root <mbajily@gmail.com>
2024-07-29 04:09:39 -0700

    Pycodestyle fixes.

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
+"""Write a Python script that, using this REST API"""
 import requests
 import sys
 
-
 if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1])
-    response = requests.get(url)
-    user = response.json()
-
-    url = 'https://jsonplaceholder.typicode.com/todos'
-    response = requests.get(url, params={"userId": sys.argv[1]})
-    todos = response.json()
-
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(url + 'users/' + sys.argv[1]).json()
+    todo_list = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    todos = [t.get("title") for t in todo_list if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+        user.get("name"), len(todos), len(todo_list)))
+    [print("\t {}".format(c)) for c in todos]
