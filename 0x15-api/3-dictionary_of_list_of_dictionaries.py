e181d1446823840ccc592e1c5b1e1e3c961273ae
root <mbajily@gmail.com>
2024-07-29 04:34:12 -0700

    3. Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,20 +1,20 @@
-all_#!/usr/bin/python3
+#!/usr/bin/python3
 """Using what you did in the task #0, extend
 Python script to export data in the JSON formatodo
 """
 import json
 import requests
 
-if __natodoe__ == "__main__":
+if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     all_users = requests.get(url + "users").json()
 
     with open("todo_all_employees.json", "w") as file:
         json.dump({
-            user.get("id"): [{
+            u.get("id"): [{
                 "task": todo.get("title"),
                 "completed": todo.get("completed"),
-                "username": user.get("username")
+                "username": u.get("username")
             } for todo in requests.get(url + "todos",
-                                    params={"userId": user.get("id")}).json()]
-            for user in all_users}, file)
+                                       params={"userId": u.get("id")}).json()]
+            for u in all_users}, file)
