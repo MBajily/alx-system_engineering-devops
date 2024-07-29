5b52f3c73cb255c408f306ca43fea86e586ac491
Mohammed Omar <mbajily@gmail.com>
2024-07-29 14:20:39 +0300

    Using what you did in the task #0, extend your Python script to export data in the JSON format.

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Using what you did in the task #0, extend
+Python script to export data in the JSON format.
+"""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    userID = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}" + str(userID)).json()
+    todo = requests.get(url + "todos", params={"userId": userID}).json()
+
+    username = user.get("username")
+    with open("{}.json".format(userID), "w") as file:
+        json.dump({userID: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todo]}, file)
