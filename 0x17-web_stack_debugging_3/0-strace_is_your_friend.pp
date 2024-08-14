0861173dc80548084a3658b3723bd3660aac4a04
Mohammed Omar <mbajily@gmail.com>
2024-08-14 17:08:04 +0300

    0x17. Web stack debugging #3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+exec { 'fix-wordpress':
+  command => 'sed -i "s/\.phpp/\.php/" /var/www/html/wp-settings.php',
+  path    => ['/usr/bin', '/bin'],
+  onlyif  => 'grep -q "\.phpp" /var/www/html/wp-settings.php',
+}
