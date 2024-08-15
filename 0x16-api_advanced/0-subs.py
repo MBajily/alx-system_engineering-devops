f51b14db28752024a84d6a81c9eee77fb4350b79
root <mbajily@gmail.com>
2024-08-15 08:12:04 -0700

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-"""Module for retrieving subscriber count from Reddit subreddits."""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Fetch and return the total number of subscribers for a given subred
-    """
-    endpoint = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    header = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
-    response = requests.get(endpoint, headers=header, allow_redirects=False)
-
-    if response.status_code == 404:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         return 0
-
-    subscriber_count = response.json().get("data")
-    return subscriber_count.get("subscribers")
+    results = response.json().get("data")
+    return results.get("subscribers", 0)
