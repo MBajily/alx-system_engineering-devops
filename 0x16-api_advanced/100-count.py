b83a577896f7d8ed3ddf12814a8e63edcdc60641
root <mbajily@gmail.com>
2024-08-15 08:19:31 -0700

    3. Count it!

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,7 +5,7 @@ import requests
 
 def count_words(subreddit, word_list, after=None, count_dict=None):
     """
-    Recursively query the Reddit API, parse hot article titles, and count keywords.
+    Recursively query Reddit API, parse hot article titles, and count keywords.
 
     Args:
         subreddit (str): The subreddit to search.
@@ -23,7 +23,8 @@ def count_words(subreddit, word_list, after=None, count_dict=None):
     headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
     params = {"limit": 100, "after": after}
 
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    response = requests.get(url, headers=headers,
+                            params=params, allow_redirects=False)
 
     if response.status_code != 200:
         return
@@ -51,6 +52,7 @@ if __name__ == '__main__':
 
     if len(sys.argv) < 3:
         print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
-        print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
+        print("Ex: {} programming\
+                'python java javascript'".format(sys.argv[0]))
     else:
         count_words(sys.argv[1], [x for x in sys.argv[2].split()])
